3ecc89ef36c5279b6e8fed7a20a4f8cc3998f199
Lucas Hillier <lucaskhillier@gmail.com>
2025-03-10 23:58:36 -0400

    Add routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,31 @@
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 import Home from './home';
 import TodoType from '@/types/Todo';
 import Add from './add';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const RootStack = () => {
+    return (
+        <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen
+                name="Home"
+                component={Home}
+                options={{ title: 'Home' }}
+            />
+            <Stack.Screen
+                name="Add"
+                component={Add}
+                options={{ title: 'Add To-Do' }}
+            />
+        </Stack.Navigator>
+    );
+};
+
+export const TodoContext = createContext<TodoType[]>([]);
 
 const Index = () => {
     const [todos, setTodos] = useState<TodoType[]>([]);
@@ -16,8 +39,11 @@ const Index = () => {
             .catch((error) => console.log(error));
     }, []);
 
-    // return <Home todos={todos} />;
-    return <Add />;
+    return (
+        <TodoContext.Provider value={todos}>
+            <RootStack />
+        </TodoContext.Provider>
+    );
 };
 
 export default Index;
